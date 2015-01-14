42b71a05e84f64d4fc342dff826d46822354718e
Piotr Sobczyk <sobbombo@gmail.com>
2015-01-14 10:18:59 +0100

    dodanie analizy glosow niewaznych

--- analiza_klimek.R
+++ analiza_klimek.R
@@ -9,9 +9,10 @@ dane.fsd <- dane.plik %>%
   filter(partia %in% duze.partie,
          wyborcy > 100) %>%
   mutate(wynik       = 100*glosy/oddane,
+         niewazne    = 100*(1-wazne/oddane),
          frekwencja  = 100*oddane/wyborcy,
          wojewodztwo = lista.wojwodztw[floor(kod/10000)/2]) %>%
-  dplyr::select(wojewodztwo, wyborcy, frekwencja, partia, wynik)
+  dplyr::select(wojewodztwo, wyborcy, frekwencja, niewazne, partia, wynik, glosy)
 
 neworder <- lista.wojwodztw[c(2,3,5,13,7,6, 10,9,4,8,14,16,11,12,15,1)]
 png("wyniki_frekwencja_2014.png", width = 1000, height = 1400)
@@ -64,3 +65,23 @@ summary(lm.fit)
 
 lm.fit2 <- lm(dane.fsd$wynik~dane.fsd$frekwencja*dane.fsd$partia)
 summary(lm.fit2)
+
+#postaramy się teraz odpowiedzieć na podobne pytania, ale w kontekście 
+#procenta głosów nieważnych
+a <- aggregate(dane.fsd$glosy, by=list(dane.fsd$partia, dane.fsd$wojewodztwo), sum)
+b <- aggregate(dane.fsd$wyborcy*dane.fsd$frekwencja/100, by=list(dane.fsd$partia, dane.fsd$wojewodztwo), sum)
+dummy2 <- data.frame(cbind(a[,1:2],100*a[,3]/b[,3]))
+colnames(dummy2) <- c("partia", "wojewodztwo", "Z")
+ggplot(transform(dane.fsd,
+                 wojewodztwo=factor(wojewodztwo,levels=neworder)), aes(y=wynik, x=niewazne)) + 
+  geom_point(shape=20, alpha=1/4, size = 0.5, col = "red") +  
+  facet_grid(wojewodztwo ~ partia)+
+  geom_hline(data = dummy2, aes(yintercept = Z)) +
+  theme(strip.text.y = element_text(size=12, face="bold"),
+        strip.background = element_rect(colour="red", fill="#CCCCFF")) +
+  xlab("Frekwencja w %") +
+  ylab("Poparcie w %") +
+  ggtitle("Wyniki wyborów samorzadowych 2014")
+
+lm.fit3 <- lm(dane.fsd$wynik~dane.fsd$frekwencja*dane.fsd$niewazne*dane.fsd$partia)
+summary(lm.fit3) 
